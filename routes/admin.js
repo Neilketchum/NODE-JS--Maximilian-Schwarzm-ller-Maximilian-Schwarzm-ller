98af2673275ddaa609ca103f54a23f96e17f58bc
Neilketchum <daipayanh@gmail.com>
2020-06-16 00:22:23 +0530

    First Commit

---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+const router = express.Router();
+const path = require('path')
+const rootDir = require('../utils/path')
+router.use('/add-product',(req,res,next)=>{
+   res.sendFile(path.join(rootDir,'views','add-product.html'))
+})
+
+router.post('/product',(req,res,next)=>{
+    // console.log(req.body);
+    res.redirect('/admin/add-product')
+});
+module.exports = router;
